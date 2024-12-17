e639a0f9f99d2f2cad0cfd5af151590d2bcc8efa
rybijakKarpiowy <104085107+rybijakKarpiowy@users.noreply.github.com>
2024-12-17 15:53:06 +0100

    Init

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+public class Main {
+    static void printUsage() {
+        System.out.println("Usage: java Main <field_size> <farmer_count>");
+        System.out.println("Flags: -c <carrot_growth_probability> -r <rabbit_spawn_probability> --continue");
+    }
+
+    public static void main(String[] args) {
+        // parse arguments
+        if (args.length < 3) {
+            printUsage();
+            System.exit(1);
+        }
+
+        int fieldSize = Integer.parseInt(args[0]);
+        int farmerCount = Integer.parseInt(args[1]);
+
+        double carrotGrowthProbability = 0.6;
+        double rabbitSpawnProbability = 0.1;
+        boolean continueSimulation = false;
+
+        for (int i = 2; i < args.length; i++) {
+            if (args[i].equals("-c")) {
+                try {
+                    carrotGrowthProbability = Double.parseDouble(args[i + 1]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid carrot growth probability");
+                    printUsage();
+                    System.exit(1);
+                }
+                i++;
+            } else if (args[i].equals("-r")) {
+                try {
+                    rabbitSpawnProbability = Double.parseDouble(args[i + 1]);
+                } catch (NumberFormatException e) {
+                    System.out.println("Invalid rabbit spawn probability");
+                    printUsage();
+                    System.exit(1);
+                }
+                i++;
+            } else if (args[i].equals("--continue")) {
+                continueSimulation = true;
+            }
+        }
+
+        // create simulation
+
+    }
+}
