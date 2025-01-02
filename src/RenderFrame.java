69932407ab5ba259da6ee9d9b2c7c346ac0e6832
ddf172 <jakubddfadamski@gmail.com>
2025-01-02 16:55:33 +0100

    Added Borders to RenderFrame

--- src/RenderFrame.java
+++ src/RenderFrame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.basic.BasicBorders;
 import java.awt.*;
 
 public class RenderFrame extends JFrame {
@@ -9,12 +11,14 @@ public class RenderFrame extends JFrame {
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new GridLayout(rows, cols));
+        ((JPanel) getContentPane()).setBorder(new EmptyBorder(10, 10, 10, 10));
 
         panels = new JPanel[rows][cols];
 
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
                 panels[i][j] = new JPanel();
+                panels[i][j].setBorder(new BasicBorders.FieldBorder(Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK));
                 add(panels[i][j]);
             }
         }
