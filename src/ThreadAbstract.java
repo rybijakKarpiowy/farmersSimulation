387bd33bd21ff776146f4f7ea8b32b1b401dd2d8
rybijakKarpiowy <104085107+rybijakKarpiowy@users.noreply.github.com>
2025-01-13 09:54:48 +0100

    Default settings changed

--- src/ThreadAbstract.java
+++ src/ThreadAbstract.java
@@ -1,6 +1,6 @@
 public abstract class ThreadAbstract extends Thread {
     protected volatile boolean running = true;
-    static private final int tickInterval = 100;
+    static private final int tickInterval = 400;
 
     public ThreadAbstract() {
         this.start();
